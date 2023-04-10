76e1cb1ea36bb4ee4fb4442fb57bc380ff1dd1e0
Martin A. Garcia <82984398+martinagustin@users.noreply.github.com>
2023-04-10 11:18:37 +0200

    Ejercicios-Sesion1
    agregamos resolucion de los 2 ejercicios de la resolucion 1

---
+++ Ejercicios-sesion1/Ejercicio1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese su nombre: ");
+string Nombre=Console.ReadLine();
+Console.Clear();
+Console.WriteLine(Nombre);
---
+++ Ejercicios-sesion1/Ejercicio2/Program.cs
@@ -0,0 +1,3 @@
+﻿DateTime fechaActual= DateTime.Now;
+Console.WriteLine("La hora actual es: "+Environment.NewLine);
+Console.WriteLine(fechaActual.ToString("HH:mm:ss"));
