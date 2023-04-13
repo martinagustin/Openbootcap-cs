87fe29336ee64f8dbf497e875838333fcf341ab3
Martin A. Garcia <82984398+martinagustin@users.noreply.github.com>
2023-04-13 21:52:26 +0200

    sesion 5
    ejercicios 1 y 2

---
+++ Ejercicios-Sesion5/Ejercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Ejercicio 1 - If
+ Escribe un programa que:
+
+Pida datos a un cliente: Nombre, email, cupón
+
+Determine si un cliente tiene un cupon descuento
+
+Muestre un precio rebajado en función del descuento
+
+Muestre el precio de un producto sin descuento si no hay cupón
+
+Nota: puedes poner un precio fijo de un producto o uno variable.
+ */
+Console.WriteLine("Ingrese Nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese email");
+string email = Console.ReadLine();
+Console.WriteLine("tiene cupon?\ningreselo");
+double cupon = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El cupon ingresado es de {0}",cupon+"%");
+double producto;
+double fideos = 12;
+Console.WriteLine("Precio original: {0}",fideos+"$");
+if (cupon != null)
+{
+    producto=fideos*(1-(cupon/100));
+    Console.WriteLine("Precio con cupon: {0}",producto+"$");
+}
---
+++ Ejercicios-Sesion5/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Ejercicio 2 - Switch
+
+Haz una lista de lenguajes de programación, por ejemplo: C#, Java, C++. Presenta la lista en consola y pide que el usuario seleccione el lenguaje mediante 1, 2, 3 o a, b, c. Presenta el resultado en consola.
+
+Nota: puedes añadir al resultado el “Hola, mundo” para el caso de C#.
+ */
+string[] lenguajes = { "C#", "Java", "C++" };
+Console.WriteLine("Seleccione un numero\n1-\n2-\n3-",Environment.NewLine);
+Console.WriteLine();
+string opcion=Console.ReadLine();
+Console.WriteLine();
+switch (opcion)
+{
+    case "1":
+        Console.WriteLine("Su lenguaje es {0}", lenguajes[0]+Environment.NewLine);
+        Console.WriteLine("Hola Mundo");
+            break;
+    case "2":
+        Console.WriteLine("Su lenguaje es {1}", lenguajes[1]);
+        break;
+    case "3":
+        Console.WriteLine("Su lenguaje es {2}", lenguajes[2]);
+        break;
+    default:
+        break;
+}
